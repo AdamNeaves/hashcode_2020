6e25c91654321d59f53678913753df9d2f63ef48
Adam <adn2@aber.ac.uk>
2020-02-20 18:56:12 +0000

    added the thing that gets the stuff

--- library.py
+++ library.py
@@ -14,3 +14,17 @@ class Library:
         for book in self.books:
             total += self.books[book]
         return total
+
+    def book_score_remaining(self, remaining_days):
+        days_after_signup = remaining_days - self.signup
+
+        if days_after_signup > 0:
+            num_books = self.books_per_day * days_after_signup
+            if num_books > len(self.books):
+                return self.books
+            else:
+                return_dict = {}
+                for x in list(self.books)[:num_books]:
+                    return_dict[x] = self.books[x]
+                return return_dict
+        return 0
