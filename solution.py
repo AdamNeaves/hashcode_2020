be84c859cfa6dc36bd1bc2adc09909df81111de1
Josh Cunningham <Josh.Cunningham@stfc.ac.uk>
2020-02-20 19:21:51 +0000

    add solution

--- solution.py
+++ solution.py
@@ -1,17 +1,18 @@
 import library
 import reader
+import writer
 
 days_left, remaining_libs = reader.read('./inputs/a_example.txt')
 outputs = []
 while days_left > 0:
-    next_lib = sorted(remaining_libs, key=lambda x: x.book_score_remaining(days_left))[1]
-    remaining_libs.remove(next_lib)
-    remove_dupes(remaining_libs, next_lib)
-    days_left = days_left - next_lib.slider
-    outputs.append
-    pass
+    while len(remaining_libs) > 0:
+        next_lib = sorted(remaining_libs, key=lambda x: x.book_score_remaining(days_left))[0]
+        remaining_libs.remove(next_lib)
+        next_lib.books = next_lib.avail_books()
 
+        for l in remaining_libs:
+            l.remove_dupes(next_lib.books.keys())
 
-def remove_dupes(remaining_libs, next_lib):
-    for l in remaining_libs:
-        l.remove_dupes(next_lib.books.keys())
+        days_left = days_left - next_lib.signup
+        outputs.append(next_lib)
+    writer.write(outputs)
