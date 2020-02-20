95eaeab98656ac9f5e0cce3fbe45ccf2842c6d4b
TrivialAndy <andrewlister.mail@gmail.com>
2020-02-20 19:21:55 +0000

    witeme

--- writer.py
+++ writer.py
@@ -1,8 +1,11 @@
 
 
-def write(file_name, libs, books_per_lib):
+def write(file_name, libs):
     """
     param libs: list of libraries
-    param books: list of list of book ids
     """
-
+    with open(file_name, 'w') as f:
+        f.write(f'{len(libs)}\n')
+        for lib in libs:
+            f.write(f'{lib.idx} {len(libs.books)}\n')
+            f.write(f'{' '.join(lib.books)}\n')
